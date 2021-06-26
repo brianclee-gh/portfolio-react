14b6fd48f5247dbf4171a7580ff6f4d0859ee5bf
Brian <blee0613@gmail.com>
2021-06-25 22:29:13 -0700

    more css styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,21 +3,23 @@ import React from 'react';
 const AboutMe = () => {
   return (
     <section id='aboutme'>
-      <div id='aboutme-container'>
-        <div id='aboutme-title'>
-          <h2>About Me </h2>
+      <div className='diagonal-box'>
+        <div className='diagonal-content' id='aboutme-container'>
+          <div id='aboutme-title'>
+            <h2>About Me </h2>
+          </div>
+          <p> Hey, I'm Brian! I'm a former dentist turned front-end developer. My coding journey started a year ago while stuck at home
+            during the 2020 pandemic. With not much else to do, a friend recommended
+            that I learn web development through <a href='https://theodinproject.com/'>The Odin
+            Project's</a> Full Stack Javascript Cirriculum.
+            <br />
+            I got hooked. There was so much to learn and so many different ways to
+            create whatever I wanted. It was an amazing difference from the
+            monotonous worklife I was used to.
+            <br />
+            I decided to make a huge leap and am currently in Hack Reactor's
+            Software Engineering Immersive. </p>
         </div>
-        <p> Hey, I'm Brian! I'm a former dentist turned front-end developer. My coding journey started a year ago while stuck at home
-          during the 2020 pandemic. With not much else to do, a friend recommended
-          that I learn web development through <a href='https://theodinproject.com/'>The Odin
-          Project's</a> Full Stack Javascript Cirriculum.
-          <br />
-          I got hooked. There was so much to learn and so many different ways to
-          create whatever I wanted. It was an amazing difference from the
-          monotonous worklife I was used to.
-          <br />
-          I decided to make a huge leap and am currently in Hack Reactor's
-          Software Engineering Immersive. </p>
       </div>
     </section>
   )
