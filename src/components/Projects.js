e9ed77f84b5da6483d936655658d0c0db689f2bf
Brian <blee0613@gmail.com>
2021-10-02 12:04:38 -0700

    moved personal projects into projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import todolist from '../data/images/projects/todolist.png'
 import projectData from '../data/projectData'
 import ProjectCard from './ProjectCard'
 
@@ -17,77 +16,7 @@ const Projects = () => {
             </div>
           : ''
         }
-        <div id='projects-card-container'>
-          <div className='row' id='upper-row'>
-          <div className='project-card'>
-              <div className='project-card-img'>
-                <img
-                    className='portfolio-img'
-                    src=''
-                    alt='Todo List'
-                />
-              </div>
-              <div className='project-card-desc'>
-                <h3>Tic Tac Toe</h3>
-                <p>Play with your friend, a randomized computer, or a computer that can't lose!</p>
-                {/* <div>tech used</div> */}
-                <a href="https://github.com/brianclee-gh/tictactoe-minmax" alt="tictactoe">
-                  <button className='project-btn'>See Project on Github</button>
-                </a>
-              </div>
-
-
-            </div>
-
-            <div className='project-card'>
-              <div className='project-card-img'>
-                <img
-                    className='portfolio-img'
-                    src={ todolist }
-                    alt='Todo List'
-                />
-              </div>
-              <div className='project-card-desc'>
-                <h3>To-Do List</h3>
-                <p>Add, manage and keep track of all the things you need to get done!</p>
-                {/* <div>tech used</div> */}
-                <a href="https://github.com/brianclee-gh/todo_list" alt="todo">
-                  <button className='project-btn'>See Project on Github</button>
-                </a>
-              </div>
-
-
-            </div>
-          </div>
-
-          <div className='row' id='lower-row'>
-
-          <div className='project-card'>
-              <div className='project-card-img'>
-                <img
-                    className='portfolio-img'
-                    src=''
-                    alt='WoW'
-                />
-              </div>
-              <div className='project-card-desc'>
-                <h3>World of Warcraft Site</h3>
-                <p>See weekly affixes and prepare before heading into your favorite dungeons with
-                  included routes, tips and tricks, and recommended items!
-                </p>
-                {/* <div>tech used</div> */}
-                <a href="https://github.com/brianclee-gh/wow-mplus" alt="wow">
-                  <button className='project-btn'>See Project on Github</button>
-                </a>
-              </div>
-
-
-            </div>
-          </div>
-        </div>
-
-
-        </div>
+      </div>
     </section>
   )
 }
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,10 +1,13 @@
 import cubePhoto from '../data/images/projects/cube.png'
 import atelierPhoto from '../data/images/projects/atelier.png'
 import wellnessPhoto from '../data/images/projects/wellness.png'
+import tictactoePhoto from '../data/images/projects/tictactoe.png'
+import todoListPhoto from '../data/images/projects/todolist.png'
+import wowPhoto from '../data/images/projects/wow.png'
 
 const projectData = [
   {
-    title: 'CUBE',
+    title: 'Shopping Retail Portal',
     imageSrc: cubePhoto,
     linkAlt: 'CUBE product page',
     description: 'Modernized retail product website',
@@ -17,7 +20,7 @@ const projectData = [
     ]
   },
   {
-    title: 'Atelier',
+    title: 'RESTful API',
     imageSrc: atelierPhoto,
     linkAlt: 'Atelier image',
     description: 'A RESTful API built to serve product data for an e-commerce shopping site.',
@@ -32,7 +35,7 @@ const projectData = [
     ]
   },
   {
-    title: 'Unlocking Wellness',
+    title: 'Healthcare Portal',
     imageSrc: wellnessPhoto,
     linkAlt: 'Unlocking Wellness image',
     description: `A health-care web and mobile app that allows patients to find and assemble
@@ -51,7 +54,42 @@ const projectData = [
       'Yelp API',
       'Loader.io'
     ]
-  }
+  },
+  {
+    title: 'MinMax Tic Tac Toe',
+    imageSrc: tictactoePhoto,
+    linkAlt: 'Tic Tac Toe photo',
+    description: `Play with your friend, a randomized computer, or a computer that can't lose!`,
+    ghLink: 'https://github.com/brianclee-gh/tictactoe-minmax',
+    techs: [
+      'Javascript',
+    ]
+  },
+  {
+    title: 'To-Do List',
+    imageSrc: todoListPhoto,
+    linkAlt: 'To Do List photo',
+    description: `Add, manage and keep track of all the things you need to get done!`,
+    ghLink: 'https://github.com/brianclee-gh/todo_list',
+    techs: [
+      'Javascript',
+    ]
+  },
+  {
+    title: 'World of Warcraft Site',
+    imageSrc: wowPhoto,
+    linkAlt: 'WoW photo',
+    description: `See weekly affixes and prepare before heading into your favorite dungeons with
+    included routes, tips and tricks, and recommended items!`,
+    ghLink: 'https://github.com/brianclee-gh/wow-mplus',
+    techs: [
+      'React',
+      'Webpack',
+      'Babel',
+      'Raider.io API',
+      'MaterialUI'
+    ]
+  },
 ]
 
 export default projectData;
